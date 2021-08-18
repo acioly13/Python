30ed4bec1c10a1aa45f6b312db4a82cb8a2916e4
acioly <john.acioly@gmail.com>
2021-08-18 09:41:58 -0300

    melhoria

--- s6_e5.py
+++ s6_e5.py
@@ -1,9 +1,10 @@
 peso = float(input('Digite o peso dos peixes: '))
-extra = 0
-multa = 0
 if peso > 50:
     extra = peso - 50
     multa = extra * 4
-print(f'Peso: {peso}')
-print(f'Excesso: {extra}')
-print(f'Multa: {multa}')
+else:
+    extra = 0
+    multa = 0
+print(f'Peso: {peso}Kg')
+print(f'Excesso: {extra}Kg')
+print(f'Multa: R${multa}')
