0c15a4d2079bef6479bb9db22ee0de538dbf5932
acioly <joao.acioly@dmslog.com>
2022-10-14 13:31:27 -0300

    Finalizando Interface

--- faculdade/validador_cpf.py
+++ faculdade/validador_cpf.py
@@ -1,25 +1,39 @@
 # Validador CPF
+import PySimpleGUI as sg
+import sys
 
+if sys.version_info[0] == 3:
+    import tkinter as tk
+else:
+    import Tkinter as tk
+
+
+# Classe para validar CPF
 class CPF:
+    # contrutor do cpf
     def __init__(self, cpf):
         self.cpf = cpf
 
+    # metodo para validar tamanho do cpf
     def valida_tamanho_cpf(self):
         if len(self.cpf) == 11:
             return True
         else:
             return False
 
+    # metodo que verifica se o cpf só tem numeros
     def valida_caracteres_cpf(self):
         if self.cpf.isnumeric():
             return True
         else:
             return False
 
+    # metodo para validar os digitos verificadores do cpf
     def valida_digito_cpf(self):
-        soma_digito_1,  soma_digito_2 = 0, 0
+        soma_digito_1, soma_digito_2 = 0, 0
         lista = list(self.cpf)
-        if lista[0] == lista[1] == lista[2] == lista[3] == lista[4] == lista[5] == lista[6] == lista[7] == lista[8] == lista[9] == lista[10]:
+        if lista[0] == lista[1] == lista[2] == lista[3] == lista[4] == lista[5] == lista[6] == lista[7] == lista[8] == \
+                lista[9] == lista[10]:
             return False
         else:
             for i in range(0, 9):
@@ -39,6 +53,7 @@ class CPF:
             else:
                 return False
 
+    # Função para validar o CPF
     def valida_cpf(self):
         if self.valida_tamanho_cpf() and self.valida_caracteres_cpf() and self.valida_digito_cpf():
             return True
@@ -46,8 +61,28 @@ class CPF:
             return False
 
 
-cpf = CPF(input('Digite o CPF: '))
-print(cpf.valida_tamanho_cpf())
-print(cpf.valida_caracteres_cpf())
-print(cpf.valida_digito_cpf())
-print(cpf.valida_cpf())
+# Usando a biblioteca de interface  PySimpleGUI como tema DarkAmber
+sg.theme('DarkAmber')
+layout = [
+    [sg.Text('Validador de CPF', size=(30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],
+    [sg.Text('Digite apenas os numeros')],
+    [sg.Text('Digite seu CPF'), sg.InputText(), sg.Button('Validar')]]
+
+# Criando a janela
+janela = sg.Window('Validador CPF', layout)
+# Evento loop para pegar os valores e rodar os eventos
+while True:
+    event, values = janela.read()
+    # Fechar a janela
+    if event == sg.WIN_CLOSED:
+        break
+    # Se o usuário clicar no botão Validar vai verificar se está correto
+    if event == 'Validar':
+        cpf = CPF(values[0])
+        if cpf.valida_cpf():
+            sg.popup('CPF Válido')
+        else:
+            sg.popup('CPF Inválido',
+                     'Verifique se Digitou apenas os numeros corretamente')
+
+janela.close()
